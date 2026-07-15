8e05e2e2c352ebcf91b7d6a1f8fe0112d212ed78
SuperAmr13 <meromero132005@gmail.com>
2026-07-15 21:34:40 +0000

    Use Prisma Client JS1

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@/generated/prisma/client"; 
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
@@ -6,7 +6,7 @@ const globalForPrisma = global as unknown as {
 
   export const prisma =
     globalForPrisma.prisma ??
-      new PrismaClient({});
+      new PrismaClient();
 
       if (process.env.NODE_ENV !== "production") {
         globalForPrisma.prisma = prisma;
